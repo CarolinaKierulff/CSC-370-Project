0ee7074009e7f0fce7ddfe26a386d33f8a38acfa
CarolinaKierulff <32877739+CarolinaKierulff@users.noreply.github.com>
2024-06-02 13:29:04 -0700

    Update store.py

--- store.py
+++ store.py
@@ -13,16 +13,10 @@ def writes_csv() -> None:
         writer.writerow(header)            #writes the header
         
         addresses = [
-        "9546 Jefferson Street, Pitt Meadows, BC V3Y 2X7",
-        "9275 Grand Street, L'Assomption, QC J5W 9B1",
-        "17 Golf Drive, Monteregie-Est, QC J0J 8E6",
-        "9171 Ocean St., Belledune, NB E8G 4K4",
-        "5 W. Holly St., Yarmouth, NS B5A 2S6",
-        "8260 Vale Ave., Grand-Mere, QC G9T 4P5",
-        "8265 W. Gainsway Ave., Manotick, ON K4M 2A7",
-        "40 Sage Court, Durham Bridge, NB E6C 8X9",
-        "77 Hilltop Rd., Windsor, LB A2B 2B3",
-        "196 New St., Sylvan Lake, AB T4S 4T8"  
+        '9546 Jefferson Street Vancouver BC V3Y2X7', '9275 Grand Street Port Moody BC J5W9B1', 
+        '17 Golf Drive Langley BC J0J8E6', '9171 Ocean St Kelowna BC E8G4K4', '5 W. Holly St Victoria BC B5A2S6', 
+        '8260 Vale Ave Kamloops BC G9T4P5', '8265 W. Gainsway Ave Maple Ridge BC K4M2A7', 
+        '40 Sage Court North Vancouver BC E6C8X9', '77 Hilltop Rd Saanich BC A2B2B3', '196 New St New Westminster BC T4S4T8'
         ]
 
         store_ids = ['001','002','003','004','005','006','007','008','009','110']
