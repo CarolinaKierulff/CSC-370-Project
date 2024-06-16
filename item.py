ab1994901e9fa993c2f7f82d86ad56005cd0de24
CarolinaKierulff <32877739+CarolinaKierulff@users.noreply.github.com>
2024-06-15 18:39:08 -0700

    Update item.py
    Added cost attribute

--- item.py
+++ item.py
@@ -25,27 +25,30 @@ def gen_itemID():
 def get_item_info(index):
 
     water_sports_items = [
-    ["Kayak", 800], ["Stand-Up Paddleboard", 700], ["Wetsuit", 250], ["Snorkeling Gear", 100], ["Surfboard", 500],
-    ["Life Jacket", 80], ["Diving Mask and Fin", 150], ["Waterproof Dry Bag", 50], ["Inflatable Raft", 300], ["Fishing Gear", 200],
-    ["Towable Tube", 250], ["Water Ski", 400], ["Wakeboard", 400], ["Kayak Paddle", 100], ["Swim Goggle", 30],
-    ["Scuba Tank", 250], ["Underwater Camera", 350], ["Personal Flotation Device", 80], ["Water Shoe", 50], ["Hydration Pack", 60],
-    ["Sailing Equipment", 500], ["Jet Ski", 10000], ["Bodyboard", 100], ["Waterproof Watch", 150], ["Paddleboard Leash", 30],
-    ["Underwater Scooter", 1000], ["Swim Fin", 50], ["Waterproof Phone Case", 20], ["Beach Tent", 100], ["Inflatable Kayak", 500],
-    ["Marine GPS System", 500], ["Spearfishing Gear", 300], ["Sun Protection Clothing", 50], ["Boating Accessory", 100], ["Marine Binocular", 150],
-    ["Waterproof Backpack", 80], ["Dock Line and Fender", 50], ["Marine Safety Equipment", 100], ["Fish Finder", 400], ["Waterproof Bluetooth Speaker", 100],
-    ["Kiteboarding Equipment", 800], ["Floating Cooler", 50], ["Swim Cap", 10], ["Wet Bag", 30], ["Marine Radio", 150],
-    ["Fishing Kayak", 800], ["Boat Cover", 200], ["Boat Maintenance Kit", 50], ["Snorkel Vest", 50], ["Underwater Flashlight", 80],
-    ["Kayak Cart", 100], ["Boat Seat and Cushion", 150], ["Tow Rope", 50], ["Waterproof Notebook", 20], ["Oar Lock and Rowing Accessory", 50],
-    ["Floating Keychain", 10], ["Dock Cleat", 20], ["Deck Shoe", 80], ["Pontoon Boat Accessory", 200], ["Jet Ski Cover", 100]
+    ["Kayak", 800, 719.82], ["Stand-Up Paddleboard", 700, 624.29], ["Wetsuit", 250, 224.09], ["Snorkeling Gear", 100, 93.77], 
+    ["Surfboard", 500, 448.97], ["Life Jacket", 80, 71.94], ["Diving Mask and Fin", 150, 136.5], ["Waterproof Dry Bag", 50, 46.25], 
+    ["Inflatable Raft", 300, 278.95], ["Fishing Gear", 200, 186.65], ["Towable Tube", 250, 230.98], ["Water Ski", 400, 366.8], 
+    ["Wakeboard", 400, 342.65], ["Kayak Paddle", 100, 91.89], ["Swim Goggle", 30, 28.04], ["Scuba Tank", 250, 222.69], 
+    ["Underwater Camera", 350, 300.57], ["Personal Flotation Device", 80, 74.28], ["Water Shoe", 50, 43.77], ["Hydration Pack", 60, 52.95], 
+    ["Sailing Equipment", 500, 465.24], ["Jet Ski", 10000, 8692.0], ["Bodyboard", 100, 91.96], ["Waterproof Watch", 150, 132.79], 
+    ["Paddleboard Leash", 30, 28.29], ["Underwater Scooter", 1000, 945.16], ["Swim Fin", 50, 44.38], ["Waterproof Phone Case", 20, 18.85], 
+    ["Beach Tent", 100, 89.87], ["Inflatable Kayak", 500, 429.87], ["Marine GPS System", 500, 454.0], ["Spearfishing Gear", 300, 272.46], 
+    ["Sun Protection Clothing", 50, 44.16], ["Boating Accessory", 100, 91.02], ["Marine Binocular", 150, 132.24], ["Waterproof Backpack", 80, 69.44], 
+    ["Dock Line and Fender", 50, 45.2], ["Marine Safety Equipment", 100, 90.13], ["Fish Finder", 400, 360.4], ["Waterproof Bluetooth Speaker", 100, 94.36], 
+    ["Kiteboarding Equipment", 800, 696.75], ["Floating Cooler", 50, 44.31], ["Swim Cap", 10, 9.48], ["Wet Bag", 30, 28.24], ["Marine Radio", 150, 134.0], 
+    ["Fishing Kayak", 800, 707.2], ["Boat Cover", 200, 170.91], ["Boat Maintenance Kit", 50, 45.15], ["Snorkel Vest", 50, 46.2], 
+    ["Underwater Flashlight", 80, 75.42], ["Kayak Cart", 100, 92.1], ["Boat Seat and Cushion", 150, 141.86], ["Tow Rope", 50, 43.36], 
+    ["Waterproof Notebook", 20, 17.73], ["Oar Lock and Rowing Accessory", 50, 43.43], ["Floating Keychain", 10, 8.85], ["Dock Cleat", 20, 17.14], 
+    ["Deck Shoe", 80, 70.89], ["Pontoon Boat Accessory", 200, 179.61], ["Jet Ski Cover", 100, 88.35]
     ]
 
     return water_sports_items[index]
-
+    
     
 def writes_csv(arg_amount) -> None:
     with open('item.csv', 'w', newline='') as csvfile:
         
-        header = ['itemId', 'name', 'price']
+        header = ['itemId', 'name', 'price', 'cost']
         writer = csv.writer(csvfile)
         
         writer.writerow(header)            #writes the header
@@ -59,7 +62,8 @@ def writes_csv(arg_amount) -> None:
                 item_id = str(gen_itemID()) 
                 name = get_item_info(i)
                 price = get_item_info(i)
-                line = [item_id,name[0] + ' ' + colors[ran_color],price[1]] 
+                cost = get_item_info(i)
+                line = [item_id,name[0] + ' ' + colors[ran_color],price[1],cost[2]] 
                 ran_color = ran_color + 1
                 writer.writerow(line)
     return
@@ -69,7 +73,7 @@ def main():
     try:
         arg_amount = int(sys.argv[1])
         if arg_amount < 1 or arg_amount > 180:
-            print('Amount invalid: Please enter number between 1 and 20000.')
+            print('Amount invalid: Please enter number between 1 and 180.')
         else:
             writes_csv(arg_amount)
 
